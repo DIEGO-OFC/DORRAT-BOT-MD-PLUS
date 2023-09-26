090bf65308b654d66c5b67de366db5773185f272
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-09-26 18:14:18 -0400

    Add files via upload

--- plugins/play.js
+++ plugins/play.js
@@ -1,4 +1,4 @@
-const {youtubedl, youtubedlv2} = require("@bochilteam/scraper");
+//const {youtubedl, youtubedlv2} = require("@bochilteam/scraper");
 const search = require("yt-search");
 const {generateWAMessageFromContent} = require("@whiskeysockets/baileys");
 const { smsg, getGroupAdmins, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, delay, format, logic, generateProfilePicture, parseMention, getRandom, searchAndFilterVideos, formatNumber} = require('../libs/fuctions.js')
@@ -6,106 +6,18 @@ const { smsg, getGroupAdmins, formatp, tanggal, formatDate, getTime, isUrl, slee
 const play = async (conn, m, command, text) => {
 
   if (!text) return m.reply(`Ejemplo: *${prefix + command}* la mamá de liam cantando`)
+m.reply(`cargando, si solo se envía el audio en 1KB use yts seleccione su enlace y luego use #ytmp3 mas el enlace`)
+const yt = await fetch(`https://xanax-apis.online/youtube/play?query=${text}&apitoken=xanax-apis`);
+const json = await yt.json();
+const titulo = json.Title;
+const link = json.Link;
+const desc = json.Description;
+const xnx = `https://xanax-apis.online/youtube/mp3?url=${link}&apitoken=xanax-apis`
+const cap = titulo + '\n' + link + '\n' + desc
 
-  const vid = await searchAndFilterVideos(text);
-  if (!vid) throw "Video Not Found, Try Another Title";
+conn.sendMessage(m.chat, {image: {url: json.Thumb}, caption: cap}, {quoted: m});
+conn.sendMessage(m.chat, { audio: { url: xnx }, fileName:  'xd.mp3', mimetype: 'audio/mp4' }, { quoted: m })}
 
-  const {title, thumbnail, timestamp, views, ago, url} = vid;
-  const dla = "Downloading audio please wait";
-  const dls = "Downloading audio succes";
-  const captvid = `📺 *Title:* ${title ? title : 'tidak diketahui'}
-⌛ *Duration:* ${timestamp ? timestamp : 'tidak diketahui'}
-👀 *Views:* ${formatNumber(views) ? formatNumber(views) : 'tidak diketahui'}
-📅 *Upload:* ${ago ? ago : 'tidak diketahui'}
-🔗 *Link:* ${url}
-
-`;
-  const ytthumb = imagen1
-  const msg = await generateWAMessageFromContent(
-    m.chat,
-    {
-      extendedTextMessage: {
-        text: captvid,
-        jpegThumbnail: ytthumb,
-        contextInfo: {
-          mentionedJid: [m.sender],
-          externalAdReply: {
-            body: dla,
-            containsAutoReply: true,
-            mediaType: 1,
-            mediaUrl: url,
-            renderLargerThumbnail: true,
-            showAdAttribution: true,
-            sourceId: "WudySoft",
-            sourceType: "PDF",
-            previewType: "PDF",
-            sourceUrl: url,
-            thumbnail: ytthumb,
-            thumbnailUrl: thumbnail,
-            title: "©-SħȺđøwɃøŧ-V༂̆̆̆̆̆̆̆̆̆̆̆̊̆̆̆̆̆̂̆̆̆̂̆̆̆̆̆̆̆̊̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̂̊̆̆̆̆̆̆̆̆̂̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆̆5-MĐ"
-          }
-        }
-      },
-    },
-    {
-      quoted: m
-    }
-  );
-  await conn.relayMessage(m.chat, msg.message, {});
-  const yt = await youtubedl(url);
-  if (command == "playmp4") {
-    const link = await yt.video["144p"].download() ||
-      await yt.video["240p"].download() ||
-      await yt.video["360p"].download() ||
-      await yt.video["480p"].download() ||
-      await yt.video["720p"].download() ||
-      await yt.video["1080p"].download() ||
-      await yt.video["auto"].download();
-    const doc = {
-      video: {
-        url: link
-      },
-      mimetype: "video/mp4",
-      caption: title,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          mediaType: 2,
-          mediaUrl: url,
-          title: title,
-          body: dls,
-          sourceUrl: url,
-          thumbnail: ytthumb
-        }
-      }
-    };
-
-    await conn.sendMessage(m.chat, doc, {
-      quoted: m
-    });
-  } else {
-    const link = await yt.audio["128kbps"].download();
-    const doc = {
-      audio: {url: link},
-      mimetype: "audio/mp4",
-      fileName: title,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          mediaType: 2,
-          mediaUrl: url,
-          title: title,
-          body: dls,
-          sourceUrl: url,
-          thumbnail: ytthumb
-        }
-      }
-    };
-
-    await conn.sendMessage(m.chat, doc, {
-      quoted: m
-    });
-  }
-};
+  
 
 module.exports = { play };
