197546292f943c1e3c52b42db387175904bd4752
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-06-01 13:03:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -560,28 +560,57 @@ await conn.sendMessage(m.chat, {image: img1, caption: `*𝙷𝙾𝙻𝙰 𝚄�
   
   case 'instalarbot': case 'crearbot':   
   if (global.db.data.users[m.sender].registered < true) return reply(info.registra)  
-  conn.sendMessage(m.chat, { text: `┎┈┈┈┈┈┈┈┈┈┈┈┈┈┈  
-  \`\`\`COMO INSTALAR ESTE BOT?\`\`\`  
-  \`\`\`Este bot es nuevo todavía no se puede instalar si quiere probar sus funciones entra al grupo del bot\`\`\`  
-  *Escribe: ${prefix}grupos*  
-    
-  \`\`\`o puede probar las funcion de hacerte un sub bot Escribe: ${prefix}serbot\`\`\`  
-  ┖┈┈┈┈┈┈┈┈┈┈┈┈┈┈`,  
-  contextInfo:{  
-  mentions: [sender],   
-  forwardingScore: 9999999,  
-  isForwarded: true,   
-  "externalAdReply": {  
-  "showAdAttribution": true,  
-  "containsAutoReply": true,  
-  "title": ` ${wm}`,  
-  "body": `${pushname}`,  
-  "previewType": "PHOTO",  
-  "thumbnailUrl": ``,  
-  "thumbnail": imagen1,   
-  "sourceUrl": md}}},  
-  { quoted: m})  
-  break  
+let text = `
+*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝚃𝙴𝚁𝙼𝚄𝚇*:
+https://youtu.be/DKo7PO2ta4o
+*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
+termux-setup-storage
+> apt update 
+> pkg upgrade 
+> pkg install git -y
+> pkg install nodejs -y
+> pkg install ffmpeg -y
+> pkg install imagemagick -y
+> pkg install yarn
+> git clone https://github.com/DIEGO-OFC/DORRAT-BOT-MD-PLUS
+> cd DORRAT-BOT-MD-PLUS
+> yarn install 
+> npm install
+> npm install 
+> npm start
+
+*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙸𝙽𝙵𝙸𝙽𝙸𝚃𝚈-𝚆𝙰 𝙷𝙾𝚂𝚃𝙸𝙽𝙶 𝟸𝟺/𝟽*:
+https://youtu.be/o2Ki6tjKF1U?si=bBIUUio7hruMmVPo
+
+*—◉ 𝙿𝙰𝙶𝙸𝙽𝙰 𝙾𝙵𝙸𝙲𝙸𝙰𝙻:*
+https://www.infinity-wa.xyz/
+
+*—◉ 𝙳𝙰𝚂𝙷𝙱𝙾𝙰𝚁𝙳:*
+https://dashboard.infinitywa.xyz
+
+*—◉ 𝙿𝙰𝙽𝙴𝙻:*
+https://store.panel-infinitywa.store
+
+*—◉ 𝙳𝚄𝙳𝙰𝚂 𝚄𝙽𝙸𝙲𝙰𝙼𝙴𝙽𝚃𝙴 𝚂𝙾𝙱𝚁𝙴 𝙴𝙻 𝙷𝙾𝚂𝚃:* 
+https://discord.com/invite/JNJYqP52 
+
+*—◉ 𝙶𝚁𝚄𝙿𝙾 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿:*
+https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
+
+*—◉  𝙲𝙰𝙽𝙰𝙻 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿:*
+https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A
+`.trim();
+await conn.sendMessage(m.chat, { image: { url: "https://telegra.ph/file/dde9bd1f999297449d139.jpg", },  caption: text, contextInfo: {externalAdReply: {  
+            title: "𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 - 𝙸𝙽𝚂𝚃𝙰𝙻𝙰𝚁𝙱𝙾𝚃",
+            body: "†𝐃⃟𝕺𝐑⃯𝐑𝐇⃯𝚵𝐓᪣𝕭⃯𝚹⃯𝐓⃤†",
+            sourceUrl: "https://github.com/DIEGO-OFC/DORRAT-BOT-MD-PLUS",  
+            mediaType: 1,  
+            showAdAttribution: true,  
+            thumbnailUrl: "https://telegra.ph/file/dde9bd1f999297449d139.jpg",  
+          },  
+        },   
+      }, { quoted: m, })
+break  
   
   case '584125778026': {  
   if (!args.join(" ")) return reply(`┗❴ ⚠ 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀 ⚠ ❵┛\n  
